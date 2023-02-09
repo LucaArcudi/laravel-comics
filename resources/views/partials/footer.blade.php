855f35662d80bf0fc95889a63cd9c2a7916dc873
LucaArcudi <arcudilu@gmail.com>
2023-02-09 02:14:06 +0100

    imp footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,75 @@
 <footer>
-    <h1>
-        My Footer
-    </h1>
+
+    <section class="top-footer">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-1">
+                    <h6>DC COMICS</h6>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+
+                    <h6>SHOP</h6>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+
+                <div class="col-1">
+                    <h6>DC</h6>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshop</li>
+                        <li>CPSC Certificate</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+
+                <div class="col-1">
+                    <h6>SITES</h6>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="bot-footer">
+        <div class="container-lg">
+            <div class="row justify-content-between py-3">
+                <div class="col-2">
+                    <button class="p-3">SIGN-UP NOW!</button>
+                </div>
+                <div class="col-6 d-flex">
+                    <span class="mt-3 me-5">FOLLOW US</span>
+                    <img class="p-2" src=" {{ Vite::asset('resources/assets/img/footer-facebook.png') }}" alt="">
+                    <img class="p-2" src=" {{ Vite::asset('resources/assets/img/footer-periscope.png') }}" alt="">
+                    <img class="p-2" src=" {{ Vite::asset('resources/assets/img/footer-pinterest.png') }}" alt="">
+                    <img class="p-2" src=" {{ Vite::asset('resources/assets/img/footer-twitter.png') }}" alt="">
+                    <img class="p-2" src=" {{ Vite::asset('resources/assets/img/footer-youtube.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
 </footer>
